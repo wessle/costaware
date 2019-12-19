Fixed the confidence intervals for the variance.

main/tests/test_moments.py
@@ -3,8 +3,10 @@ import numpy as np
 
 from math import isclose
 from scipy.special import erf, erfinv
-from scipy.stats import chi2
+from scipy.stats import chi2, norm
 
+def icdf(dist, y, df=None):
+    return dist.isf(1-y) if df is None else dist.isf(1-y, df)
 
 
 class TestWelfordAlgorithm: 
@@ -40,43 +42,47 @@ class TestWelfordAlgorithm:
 
 
         """
-        alpha = 1-1e-2
-        chi_low, chi_hi = chi2.cdf(n, alpha/2), chi2.cdf(n, 1 - alpha/2)
+        alpha = 1e-2  # set the global significance level.
+
+        # bounds for the mean confidence interval
+        normal_bounds = icdf(norm, np.array([alpha/2, 1-alpha/2]))
+
+        # boundes for the variance confidence interval --- I don't understand
+        # chi2 distributions so I am not 100% sure that this is correct.
+        chi_low, chi_hi = icdf(chi2, 1-alpha/2, df=n-1), icdf(chi2, alpha/2, df=n-1)
+
+        # set up an experiment, run the estimator for the specified number of
+        # turns
         estimator = moments.welford_estimator()
         for _ in range(n-1):
             _, _ = estimator(sequence())
+
+        # get the last value of the estimated means and variances
         est_mean, est_var = estimator(sequence())
 
-        assert np.abs(est_mean - mean) <= 3 * np.sqrt(var / n)
-        assert (n-1) * est_var / chi_low <= var \
-            and var <= (n-1) * est_var / chi_hi
+        # confidence intervals
+        mean_lower, mean_higher = est_mean * np.sqrt(var / n) * normal_bounds
+        assert mean_lower <= mean <= mean_higher
+
+        assert (n-1) * est_var / chi_low <= var  and var <= (n-1) * est_var / chi_hi
 
     def test_constant_sequence1(self):
         self.__test_distribution_sequence(
-            lambda: 1.,
-            1.,
-            0
+            lambda: 1., 1., 0
         )
         
     def test_uniform_sequence(self):
         self.__test_distribution_sequence(
-            np.random.rand,
-            0.5,
-            1/12
+            np.random.rand, 0.5, 1/12
         )
 
     def test_normal_sequence(self):
         self.__test_distribution_sequence(
-            np.random.randn,
-            0.,
-            1.
+            np.random.randn, 0., 1.
         )
 
     def test_exponential_sequence(self):
         self.__test_distribution_sequence(
-            np.random.exponential,
-            1.,
-            1.
+            np.random.exponential, 1., 1.
         )
-        
 