78c32024e9aaefdd6d6413228a365a5a11560afc
David Kraemer <david.kraemer@stonybrook.edu>
2019-12-29 23:33:40 -0500

    demo to show proof of concept (basic happy paths are working for the different environments)

---
+++ scripts/proof_of_concept.py
@@ -0,0 +1,26 @@
+import gym
+import main.core.envs as envs
+import main.core.portfolio as portfolio
+import main.core.asset as asset
+
+SIM_LEN = 1000
+
+env = envs.SortinoCostAwareEnv(
+    portfolio.Portfolio(
+        [
+            asset.Asset(1., 0.05, 0.),
+            asset.Asset(1., 0.025, 0.)
+        ], 
+        [0.5, 0.5], 
+        1000.
+    ),
+    0.
+)
+env.reset()
+
+for i in range(SIM_LEN):
+    action = [0.5, 0.5]
+    state, (reward, cost), proceed, _  = env.step(action)
+
+
+
