fa6cfe87b1e583d15068a94bced3e6b1c3ca59e7
wessle <suttlewes@gmail.com>
2020-10-21 16:10:03 -0400

    changed the costs so they make more sense given the mountain car reward is negative

--- main/experimental/mountain_car_test.py
+++ main/experimental/mountain_car_test.py
@@ -26,7 +26,7 @@ episode_len = 500
 
 # Define a cost function to be used in our cost-aware environment
 def cost_fn(state):
-    return 0.1 + max(1-state[0], 0)
+    return np.clip(state[0] + 0.7, 0.1, 1)
 
 if __name__ == '__main__':
 
