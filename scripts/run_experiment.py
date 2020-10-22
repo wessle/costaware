a690fe550f54b979ade316f8183d296fb58d61b2
David Kraemer <david.kraemer@stonybrook.edu>
2020-10-22 19:06:17 -0400

    The script scripts/run_experiment.py does run correctly now

--- scripts/run_experiment.py
+++ scripts/run_experiment.py
@@ -1,39 +1,71 @@
+import argparse
+import os
+import wesutils
+
 import main.core.agents as agents
 
-from main.utils.experiment import IOManager, ExperimentManager, generate_mdp_env
+from datetime import datetime
+from main.utils.experiment import IOManager, TrialRunner, generate_mdp_env
 
 parser = argparse.ArgumentParser(
     description='Insert script description here'
 )
 
 parser.add_argument(
-    'agent', type='str', nargs=1, choices=['ac', 'q'],
+    'agent', type=str, choices=['ac', 'q'],
     help='Agent architecture to run'
 )
 
 parser.add_argument(
-    '--config', type=str, nargs=1, default="",
-    help='YAML filename that provides the configurations for the experiment'
+    '--agent_config', type=str, default="",
+    help='YAML filename that provides the agent parameters for the experiment'
 )
 
+parser.add_argument(
+    '--env_config', type=str, default="configs/default_env_config.yml",
+    help='YAML filename that provides the environment parameters for the experiment'
+)
 
 parser.add_argument(
-    '--output', type=str, nargs=1, default='data',
+    '--output', type=str, default='data',
     help='top-level directory name that all experimental output will be saved'
 )
 
 args = parser.parse_args()
 
 AgentClass = {
-    'ac': agents.LinearAC,
-    'q':  agents.TabularQ
+    'ac': agents.LinearACAgent,
+    'q':  agents.TabularQAgent
 }
 
-if __name__ = '__main__':
+if __name__ == '__main__':
 
-    agent = AgentClass[args.agent](**args.config)
-    env   = generate_mdp_env()
-    io    = IOManager(output_dir)
+    # Easiest thing to do right now is to separate the file for the environment
+    # parameters from the file for agent parameters
+    agent_config = wesutils.load_config(args.agent_config)
+    env_config   = wesutils.load_config(args.env_config)
 
-    manager = ExperimentManager(env, agent, io)
+    states, actions, env = generate_mdp_env(**env_config)
+    agent  = AgentClass[args.agent](states, actions, **agent_config)
+
+    now = datetime.now()
+    time_str = '_'.join([
+        f'{now.year}{now.month}{now.day}',
+        f'{now.hour:02d}{now.minute:02d}{now.second:02d}'
+    ])
+
+    output_dir = os.path.join(
+        args.output,
+        '_'.join([
+            agent.title,
+            f's{len(states)}a{len(actions)}',
+            time_str
+        ])
+    )
+
+    io = IOManager(output_dir)
+    io.save_yml('agent_config.yml', agent_config)
+    io.save_yml('env_config.yml', env_config)
+
+    manager = TrialRunner(env, agent, io)
     manager.train()
