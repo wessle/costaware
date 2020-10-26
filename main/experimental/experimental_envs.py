e4eab3de263551848f1cea0da822ff45dfe40af6
Xinqi Wang <xinqi.wang32@gmail.com>
2020-10-26 14:13:51 -0400

    added acrobot_costaware env

--- main/experimental/experimental_envs.py
+++ main/experimental/experimental_envs.py
@@ -2,14 +2,39 @@ import gym
 import os
 from copy import deepcopy
 
-from util import module_from_path
-
+from main.experimental.util import module_from_path
 
 # Obtain the mountain_car module so we can subclass MountainCarEnv
 filename = deepcopy(*gym.envs.__path__) + '/classic_control/mountain_car.py'
 mountain_car_path = os.path.abspath(filename)
 MountainCarEnv = module_from_path('mountain_car',
                                   mountain_car_path).MountainCarEnv
+
+filename1 = deepcopy(*gym.envs.__path__) + '/classic_control/acrobot.py'
+acrobot_path = os.path.abspath(filename1)
+AcrobotEnv = module_from_path('acrobot',
+                                  acrobot_path).AcrobotEnv
+
+
+def cost_f1(state):
+    position = state[0]
+    if position < 0:
+        cost = -position
+    else:
+        cost = 0
+    return cost
+
+def cost_f2(state):
+    """
+    A state of [1, 0, 1, 0, ..., ...] means that both links point downwards.
+    """
+    first_link_angle = state[0]
+    if first_link_angle > 0:
+        cost = first_link_angle
+    else:
+        cost = 0
+    return cost
+
 
 class MountainCarCostAwareEnv(MountainCarEnv):
     """
@@ -24,8 +49,18 @@ class MountainCarCostAwareEnv(MountainCarEnv):
 
     def __init__(self, goal_velocity=0, cost_fn=lambda x: 1.0):
         MountainCarEnv.__init__(self, goal_velocity)
-        self.cost_fn = cost_fn
+        self.cost_fn = cost_f1
 
     def step(self, action):
         state, reward, done, d = MountainCarEnv.step(self, action)
+        return state, (reward, self.cost_fn(state)), done, d
+
+
+class AcrobotCostAwareEnv(AcrobotEnv):
+    def __init__(self):
+        AcrobotEnv.__init__(self)
+        self.cost_fn = cost_f2
+
+    def step(self, action):
+        state, reward, done, d = AcrobotEnv.step(self, action)
         return state, (reward, self.cost_fn(state)), done, d
