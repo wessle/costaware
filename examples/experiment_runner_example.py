4e057345097b4331462c49867b39f28e50ce0f6b
wessle <suttlewes@gmail.com>
2020-11-09 12:03:42 -0500

    added CL argument options

--- examples/experiment_runner_example.py
+++ examples/experiment_runner_example.py
@@ -1,19 +1,32 @@
+import argparse
+
+import data
 import main.utils.experiment as experiment
 from main.core.envs import RandomMDPEnv
 from main.core.agents import TabularQAgent
 
 
-NUM_TRIALS = 2
-CPUS_PER_TRIAL = 2
+parser = argparse.ArgumentParser(
+    'Example script showing ExperimentRunner works on MDPEnvs'
+)
+
+parser.add_argument('--num_trials', type=int, default=2,
+                    help='Number of trials to run')
+parser.add_argument('--cpus_per_trial', type=int, default=1,
+                    help='Number of CPUs to allocate to each trial')
+parser.add_argument('--output_dir', type=str,
+                    default=f'{data.__path__[0]}/experiment_runner_example',
+                    help='Directory to store trial data in')
+
+args = parser.parse_args()
 
 if __name__ == '__main__':
-    output_dirs = [f'../data/experiment_runner_example_data_{i}' \
-                   for i in range(NUM_TRIALS)]
+    output_dirs = [f'{args.output_dir}_{i}' \
+                   for i in range(args.num_trials)]
 
     env_config = {'class': RandomMDPEnv,
                   'args': [5, 5, 'r1', 'c1'],
-                  'kwargs': {'transition_seed': 1066,
-                             'training_seed': 1789}}
+                  'kwargs': {'transition_seed': 1066}}
     env = experiment.EnvConstructor().create(env_config)
 
     agent_config = {'class': TabularQAgent,
@@ -33,8 +46,8 @@ if __name__ == '__main__':
         env_config, agent_config, iomanager_config, trial_config) \
         for iomanager_config in iomanager_configs]
 
-    ray_configs = {'num_cpus': NUM_TRIALS*CPUS_PER_TRIAL, 'num_gpus': 0,
-                   'cpus_per_trial': CPUS_PER_TRIAL, 'gpus_per_trial': 0}
+    ray_configs = {'num_cpus': args.num_trials*args.cpus_per_trial, 'num_gpus': 0,
+                   'cpus_per_trial': args.cpus_per_trial, 'gpus_per_trial': 0}
 
     expr = experiment.ExperimentRunner()
     expr.register_experiment_configs(config_tuples)
