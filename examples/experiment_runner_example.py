be5a32772e7e3fdb731d989dd8d03bf0504c8f04
wessle <suttlewes@gmail.com>
2020-11-09 11:45:05 -0500

    got experiment_runner_example.py working with multiple trials, altered IOManager definition to warn the user when output_dir exists but then write into output_dir anyway

--- examples/experiment_runner_example.py
+++ examples/experiment_runner_example.py
@@ -3,37 +3,43 @@ from main.core.envs import RandomMDPEnv
 from main.core.agents import TabularQAgent
 
 
-# TODO: Get this working! Currently experiencing error regarding
-# EnvConstructor inside TrialConstructor.
+NUM_TRIALS = 2
+CPUS_PER_TRIAL = 2
 
-output_dir = 'experiment_runner_test_data'
+if __name__ == '__main__':
+    output_dirs = [f'../data/experiment_runner_example_data_{i}' \
+                   for i in range(NUM_TRIALS)]
 
-env_config = {'class': RandomMDPEnv,
-              'args': [5, 5, 'r1', 'c1'],
-              'kwargs':dict()}
-env = experiment.EnvConstructor.create(env_config)
+    env_config = {'class': RandomMDPEnv,
+                  'args': [5, 5, 'r1', 'c1'],
+                  'kwargs': {'transition_seed': 1066,
+                             'training_seed': 1789}}
+    env = experiment.EnvConstructor().create(env_config)
 
-agent_config = {'class': TabularQAgent,
-                'args': [env.states, env.actions, 0.01, 0.001],
-                'kwargs': dict()}
-
-iomanager_config = {'class': experiment.IOManager,
-                    'args': [output_dir],
+    agent_config = {'class': TabularQAgent,
+                    'args': [env.states, env.actions, 0.01, 0.001],
                     'kwargs': dict()}
 
-trial_config = {'width': 100, 'print_interval': 10000, 'n_steps': 500000,
-                'logging': True, 'plotting': False, 'stdouting': True}
+    iomanager_configs = [
+        {'class': experiment.IOManager,
+         'args': [output_dir],
+         'kwargs': dict()} \
+        for output_dir in output_dirs]
 
-config_tuple = experiment.ConfigTuple(env_config, agent_config,
-                                      iomanager_config, trial_config)
+    trial_config = {'width': 100, 'print_interval': 10000, 'n_steps': 100000,
+                    'logging': True, 'plotting': False, 'stdouting': True}
 
-ray_configs = {'num_cpus': 2, 'num_gpus': 0,
-               'cpus_per_trial': 2, 'gpus_per_trial': 0}
+    config_tuples = [experiment.ConfigTuple(
+        env_config, agent_config, iomanager_config, trial_config) \
+        for iomanager_config in iomanager_configs]
 
-expr = experiment.ExperimentRunner()
-expr.register_experiment_configs([config_tuple])
-expr.register_ray_configs(ray_configs)
-expr.verify_configs()
-expr.initialize_ray()
-expr.run_experiment()
-expr.shutdown_ray()
+    ray_configs = {'num_cpus': NUM_TRIALS*CPUS_PER_TRIAL, 'num_gpus': 0,
+                   'cpus_per_trial': CPUS_PER_TRIAL, 'gpus_per_trial': 0}
+
+    expr = experiment.ExperimentRunner()
+    expr.register_experiment_configs(config_tuples)
+    expr.register_ray_configs(ray_configs)
+    expr.verify_configs()
+    expr.initialize_ray()
+    expr.run_experiment()
+    expr.shutdown_ray()
