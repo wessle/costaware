9a22a6437c19c44b9127254d1b8eaa46e1270ba9
Xinqi Wang <xinqi.wang@stonybrook.edu>
2020-11-17 15:31:06 -0500

    New d (#33)
    * added costaware version of the pendulum env.
    
    * new cost function and modified state observation
    
    * observation space
    
    * added get_ob functions
    
    Co-authored-by: Wesley Suttle <wessle@users.noreply.github.com>

--- main/experimental/pendulum_test.py
+++ main/experimental/pendulum_test.py
@@ -1,18 +1,17 @@
 import numpy as np
-from numpy import cos, sin
 import wesutils
 from time import time
 
 from main.core import agents
-from main.experimental.experimental_envs import AcrobotCostAwareEnv
+from main.experimental.experimental_envs import PendulumCostAwareEnv
 
 # network and agent parameters
-Q_hidden_units = 256
+Q_hidden_units = 64
 buffer_maxlen = 100000
-batchsize = 128
+batchsize = 64
 q_lr = 0.001
 rho_lr = 0.0001
-eps = 0.1
+eps = 0.05
 enable_cuda = False
 rho_init = 0
 grad_clip_radius = None
@@ -23,46 +22,28 @@ num_episodes = 100
 episode_len = 300
 
 
-# 11/17 learns in ~10 episodes (best rw = -74)
-# not very stable
+# Define a cost function to be used in our cost-aware environment
 def cost_fn(state):
     """
-    A state of [1, 0, 1, 0, ..., ...] means that both links point downwards.
-    swung height = -cos(state[0]) - cos(state[1] + state[0])
+    state = [cos(theta), sin(theta), ang_vel]
     """
-    height = -cos(state[0]) - cos(state[1] + state[0])
-    if state[0] > 0:
-        # First line is below horizontal, maximize the second angle
-        # (between first and second link)
-        cost = max(1-state[2], 0.1) ** 2
-    else:
-        # first link is above horizontal, give higher cost
-        # for higher height reached
-        cost = (1+height) ** 2
-    return cost
-
-
-# 11/17 learns in ~20 episodes
-def cost_fn1(state):
-    height = -cos(state[0]) - cos(state[1] + state[0])
-    # Height > 0, give greater cost for higher point
-    if height > 0:
-        cost = (max(1 + height, 1.2))**2
-    # OW give cost according to first angle
-    else:
-        cost = (1 - (state[0]/5))**2
-    return cost
+    cost1 = 1 + state[0] / 2
+    cost2 = 1 - abs(state[2] / 9)
+    # give cost based on the theta value and the angular velocity
+    # if the theta is ~1 (standing vertical)
+    # give higher cost for smaller angular velocity
+    return (0.6*cost1 + 0.4*cost2)**3
 
 
 if __name__ == '__main__':
 
     # create env
-    env = AcrobotCostAwareEnv(cost_fn = cost_fn)
+    env = PendulumCostAwareEnv(cost_fn=cost_fn)
     env.reset()
 
     # gather info about the env
     state_dim = env.observation_space.shape[0]
-    num_actions = env.action_space.n
+    num_actions = len(env.action_space.shape)
     assert np.ndim(env.action_space.shape) == 1, 'action space must be 1D'
     action_dim = 1
 
@@ -82,7 +63,7 @@ if __name__ == '__main__':
     # create formats for printing output
     fmt = '{:^5s} | {:^10s} | {:^10s} | {:^10s} | {:^10s} | {:^10s} | {:^10s}'
     fmt_vals = '{:^5} | {:^10.2f} | {:^10.2f} | {:^10.2f} | {:^10.2f} | ' + \
-            '{:^10.2f} | {:^10.2f}'
+               '{:^10.2f} | {:^10.2f}'
 
     # run the experiment
     end_values, rhos = [], []
@@ -91,6 +72,7 @@ if __name__ == '__main__':
         t0 = time()
         for _ in range(episode_len):
             action = agent.sample_action(env.get_ob())
+            action = [action]
             state, reward_cost_tuple, done, _ = env.step(action)
             reward, cost = reward_cost_tuple
             rewards.append(reward)
@@ -99,7 +81,7 @@ if __name__ == '__main__':
             if done:
                 break
 
-        # safe info and print update
+        # save info and print update
         end_values.append((np.sum(rewards), np.sum(costs)))
         rhos.append(np.mean(rewards) / np.mean(costs))
 
@@ -107,6 +89,6 @@ if __name__ == '__main__':
             print(fmt.format(
                 'ep', 'rew', 'cost', 'time(s)', 'rho', 'val_est', 'Vsref'))
         print(fmt_vals.format(i, *end_values[-1], time() - t0,
-            rhos[-1], agent.ref_val_est, agent.ref_state_val()))
+                              rhos[-1], agent.ref_val_est, agent.ref_state_val()))
 
         env.reset()
