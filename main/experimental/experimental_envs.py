b72a51a51560f2e7251f02886c534b199b7afb96
Xinqi Wang <xinqi.wang32@gmail.com>
2020-12-03 21:02:32 -0500

    added cartpole env.

--- main/experimental/experimental_envs.py
+++ main/experimental/experimental_envs.py
@@ -1,5 +1,6 @@
 import gym
 import os
+import numpy as np
 from copy import deepcopy
 
 from main.experimental.util import module_from_path
@@ -16,6 +17,10 @@ AcrobotEnv = module_from_path('acrobot', acrobot_path).AcrobotEnv
 pendulum_file = deepcopy(*gym.envs.__path__) + '/classic_control/pendulum.py'
 pendulum_path = os.path.abspath(pendulum_file)
 PendulumEnv = module_from_path('pendulum', pendulum_path).PendulumEnv
+
+cartpole_file = deepcopy(*gym.envs.__path__) + '/classic_control/cartpole.py'
+cartpole_path = os.path.abspath(cartpole_file)
+CartPoleEnv = module_from_path('cartpole', cartpole_path).CartPoleEnv
 
 
 class MountainCarCostAwareEnv(MountainCarEnv):
@@ -82,3 +87,28 @@ class PendulumCostAwareEnv(PendulumEnv):
 
     def get_ob(self):
         return PendulumEnv._get_obs(self)
+
+
+class CartPoleCostAwareEnv(CartPoleEnv):
+    """
+        Extension of the OpenAI CartPoleEnv to include a cost as well as
+        a reward. Used to test RL algorithms that maximize the ratio of long-run
+        average reward over long-run average cost.
+
+        User can pass in a cost function that takes the current state as input
+        and outputs a corresponding cost. Default cost function is 1.0, reducing
+        to the reward-only case.
+
+        * this environment returns +1 for each step that is not termination
+        """
+
+    def __init__(self, cost_fn=lambda x: 1.0):
+        CartPoleEnv.__init__(self)
+        self.cost_fn = cost_fn
+
+    def step(self, action):
+        state, reward, done, d = CartPoleEnv.step(self, action)
+        return state, (reward, self.cost_fn(state)), done, d
+
+    def get_ob(self):
+        return np.array(self.state)
